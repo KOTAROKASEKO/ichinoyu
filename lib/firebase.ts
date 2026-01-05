849607ff1edff6d58abbb843c1a9435ff3f6cb73
Kotaro Kase <kotarokase@Kotaros-MacBook-Air.local>
2026-01-05 20:57:26 +0900

    q

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,14 @@
+// lib/firebase.ts
 "use client";
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDUOh1zXg48NfR9GSIw_ZxkkDuWDFtZ5s",
-  authDomain: "ichinoyu.firebaseapp.com",
-  projectId: "ichinoyu",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
 };
 
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const db = getFirestore(app);
